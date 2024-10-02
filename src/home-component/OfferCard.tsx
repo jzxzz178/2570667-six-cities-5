58289af43f45e5d4dd9cc97f906919f542edb01e
Fedor <pichuginf2016@mail.ru>
2024-10-02 20:42:33 +0500

    started work

---
+++ src/home-component/OfferCard.tsx
@@ -0,0 +1,64 @@
+interface OfferCardProps {
+    isPremium: boolean;
+    image: string;
+    price: number;
+    title: string;
+    type: string;
+    isBookmarked: boolean;
+    rating: number;
+}
+
+function OfferCard({
+    isPremium,
+    image,
+    price,
+    title,
+    type,
+    isBookmarked,
+    rating,
+}): JSX.Element {
+    return (
+        <article className="cities__card place-card">
+            {isPremium && (
+                <div className="place-card__mark">
+                    <span>Premium</span>
+                </div>
+            )}
+            <div className="cities__image-wrapper place-card__image-wrapper">
+                <a href="#">
+                    <img className="place-card__image" src={image} width="260" height="200" alt="Place image" />
+                </a>
+            </div>
+            <div className="place-card__info">
+                <div className="place-card__price-wrapper">
+                    <div className="place-card__price">
+                        <b className="place-card__price-value">&euro;{price}</b>
+                        <span className="place-card__price-text">&#47;&nbsp;night</span>
+                    </div>
+                    <button
+                        className={`place-card__bookmark-button button ${isBookmarked ? 'place-card__bookmark-button--active' : ''
+                            }`}
+                        type="button"
+                    >
+                        <svg className="place-card__bookmark-icon" width="18" height="19">
+                            <use xlinkHref="#icon-bookmark"></use>
+                        </svg>
+                        <span className="visually-hidden">{isBookmarked ? 'In bookmarks' : 'To bookmarks'}</span>
+                    </button>
+                </div>
+                <div className="place-card__rating rating">
+                    <div className="place-card__stars rating__stars">
+                        <span style={{ width: `${(rating / 5) * 100}%` }}></span>
+                        <span className="visually-hidden">Rating</span>
+                    </div>
+                </div>
+                <h2 className="place-card__name">
+                    <a href="#">{title}</a>
+                </h2>
+                <p className="place-card__type">{type}</p>
+            </div>
+        </article>
+    );
+};
+
+export default OfferCard;
