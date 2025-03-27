fb633a0bf3c664c8f9699c86abc9a25ffe329b6f
Fedor Pichugin <pf2772@gmail.com>
2025-03-27 14:02:50 +0500

    configures axios instance

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-import appReducer from './reduser';
+import appReducer from './reducer';
+import { createAPI } from '../api';
 
-const store = configureStore({
+const api = createAPI();
+
+export const store = configureStore({
   reducer: {
     app: appReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
 
 export default store;
